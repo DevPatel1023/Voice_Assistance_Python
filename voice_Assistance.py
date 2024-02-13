71bff7c8d453e31a4326257739369e3df125448c
Dev Patel <118754663+DevPatel1023@users.noreply.github.com>
2024-02-13 21:34:38 +0530

    Update voice_Assistance.py

--- voice_Assistance.py
+++ voice_Assistance.py
@@ -4,12 +4,17 @@ import speech_recognition as sr
 import webbrowser
 import random
 import datetime
+import pyjokes
+import requests
+
+NEWS_API_KEY = 'Your_APi_Key'
 
 WAKE_WORD = "Hey Jarvis"
 
 def takecommand():
     r = sr.Recognizer()
-    r.pause_threshold = 0.6
+    r.energy_threshold = 4000  
+    r.pause_threshold = 0.5
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
@@ -22,33 +27,43 @@ def takecommand():
             print(e)
             return "Some error occurred"
 
-
-
 def say(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
 
 def tell_joke():
-    jokes = [
-        "Why did the scarecrow win an award? Because he was outstanding in his field!",
-        "I'm reading a book on anti-gravity. It's impossible to put down!",
-        "Why don't scientists trust atoms? Because they make up everything!",
-        "What do you call a fish with no eyes? Fsh",
-        "I told my computer I needed a break, and now it won't stop sending me Kit Kat bars.",
-        "Why did the scarecrow win an award? Because he was outstanding in his field!",
-        "I don't trust stairs because they're always up to something."
-    ]
-    say(random.choice(jokes))
-
+    joke = pyjokes.get_joke()
+    say(joke)
 
 def search_the_web(query):
     search_url = f"https://www.google.com/search?q={query.replace(' ', '+')}"
     webbrowser.open(search_url)
     say(f"Searching the web for {query}.")
 
+
+
 def play_music(file_path):
     os.startfile(file_path)
+
+def get_headlines():
+    # Fetch latest news headlines
+    query_params = {
+        "source": "bbc-news",
+        "sortBy": "top",
+        "apiKey": NEWS_API_KEY
+    }
+    main_url = "https://newsapi.org/v1/articles"
+
+    # Fetching data in json format
+    res = requests.get(main_url, params=query_params)
+    open_bbc_page = res.json()
+
+    # Getting all articles' titles
+    articles = open_bbc_page["articles"]
+    headlines = [article["title"] for article in articles][:3]  # Limit to top 3 headlines
+
+    return headlines
 
 if __name__ == "__main__":
     assistant_activated = False
@@ -80,7 +95,16 @@ if __name__ == "__main__":
                 music_file_path = "C:/Users/Dev/Downloads/Kaleo - Way Down We Go.mp3"
                 play_music(music_file_path)
 
-            elif "the time" in query:
+            elif "what's the news" in query:
+                # Fetch latest news headlines
+                headlines = get_headlines()
+
+                # Speak out the headlines
+                for idx, headline in enumerate(headlines, start=1):
+                    print(f"{idx}. {headline}")
+                    say(headline)
+
+            elif "what's the time" in query:
                 current_datetime = datetime.datetime.now()
                 current_time = current_datetime.time()
                 say(f"Sir time is: {current_time}")
