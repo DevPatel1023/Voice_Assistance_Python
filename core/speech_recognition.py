fecf20b658192c397c3c449b5b4937ab0762e80a
DevPatel1023 <devp4143@gmail.com>
2025-08-31 23:10:52 +0530

    feat : speak and listen functions

--- core/speech_recognition.py
+++ core/speech_recognition.py
@@ -2,7 +2,23 @@
 
 import speech_recognition as sr
 
-
-
 #create a speech recognition object
 r = sr.Recognizer()
+
+def transcribe_audio():
+    #use audio 
+    with sr.Microphone() as source:
+        print("Listening...")
+        # record voice
+        audio_listened = r.listen(source)
+        try:
+            query = r.recognize_google(audio_listened)
+            return query.lower()
+        except sr.UnknownValueError:
+            print("Sorry, i dont understand")
+        except sr.RequestError:
+            print("Network error")
+
+    
+
+
--- core/text_to_speech.py
+++ core/text_to_speech.py
@@ -3,4 +3,7 @@
 import pyttsx3 
 
 def jarvis_output_speak(text):
-    
+    # init the engine
+    engine = pyttsx3.init()
+    engine.say("boss : ",text)
+    engine.runAndWait();
